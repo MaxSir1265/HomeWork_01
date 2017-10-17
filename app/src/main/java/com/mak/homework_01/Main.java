f28b2a85ddd0289368864577c0fdfda4a6471d51
MaxSir1265 <epresed95@gmail.com>
2017-10-17 20:07:49 +0300

    SC4

--- app/src/main/java/com/mak/homework_01/Main.java
+++ app/src/main/java/com/mak/homework_01/Main.java
@@ -1,7 +1,10 @@
 package com.mak.homework_01;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main extends AppCompatActivity {
 
@@ -10,4 +13,25 @@ public class Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClickSet(View view){
+        Intent intent;
+        switch (view.getId()){
+            case R.id.button13:
+                intent = new Intent(Main.this, AppActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.buttonCall:
+                intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"));
+                startActivity(intent);
+                break;
+            case R.id.buttonSend:
+                intent = new Intent(Intent.ACTION_VIEW, Uri.fromParts("sms", "0675655490", null));
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
+
 }
