c0ef1875c1ed74bbb927217394719344cc005921
MaxSir1265 <epresed95@gmail.com>
2017-10-17 19:29:48 +0300

    SC3

---
+++ app/src/main/java/com/mak/homework_01/AppActivity.java
@@ -0,0 +1,13 @@
+package com.mak.homework_01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app);
+    }
+}
